16e21f0987b3250d9c47725acb508e321bb4beb3
Saravanan <spoosanthiram@gmail.com>
2025-12-19 10:32:04 -0500

    AL-27 Add hello thread

---
+++ sandbox/c++_threads/hello.cpp
@@ -0,0 +1,16 @@
+#include <chrono>
+#include <iostream>
+#include <thread>
+
+void hello()
+{
+    std::cout << "Hello World!\n";
+}
+
+int main()
+{
+    std::thread t{hello};
+    t.join();
+
+    return 0;
+}
